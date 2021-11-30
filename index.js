dd5a86fe79cb4db2261f19b68ef56fc78a796dd3
Jairosll <jairosll@gmail.com>
2021-11-29 19:43:58 -0500

    Actualización 29112021, cambio de servidor DB

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const cors = require('cors'); 
 const express = require('express');
+
 const mongoose = require('mongoose');
 const convocatoriaRoutes = require('./routes/convocatoria.js');
 
 // create the express instace and asign the port
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 // create the mongoose connection for the local database (for deployment use the mongoDB cloud url)
-mongoose.connect("mongodb+srv://dbUser:toor@cluster0.xz3sa.mongodb.net/Convocatorias?retryWrites=true&w=majority",{useNewUrlParser: true}).then(() => {
+// mongodb+srv://jairosll:LaVidaEsBella@cluster0.6gpiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// mongodb+srv://dbUser:toor@cluster0.xz3sa.mongodb.net/Convocatorias?retryWrites=true&w=majority
+mongoose.connect("mongodb+srv://jairosll:LaVidaEsBella@cluster0.6gpiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser: true}).then(() => {
     console.log("successfully connected to db");
   })
   .catch((err) => {
