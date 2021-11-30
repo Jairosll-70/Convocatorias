91c303d674ea70f822c14d864ef543a759c390d4
Jairo Sanchez Llanos <90632748+minticformador18@users.noreply.github.com>
2021-11-30 18:50:33 -0500

    b23Node

---
+++ index.js
@@ -0,0 +1,29 @@
+const cors = require('cors'); 
+const express = require('express');
+
+const mongoose = require('mongoose');
+const convocatoriaRoutes = require('./routes/convocatoria.js');
+
+// create the express instace and asign the port
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// create the mongoose connection for the local database (for deployment use the mongoDB cloud url)
+// mongodb+srv://jairosll:LaVidaEsBella@cluster0.6gpiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// mongodb+srv://dbUser:toor@cluster0.xz3sa.mongodb.net/Convocatorias?retryWrites=true&w=majority
+mongoose.connect("mongodb+srv://jairosll:LaVidaEsBella@cluster0.6gpiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser: true}).then(() => {
+    console.log("successfully connected to db");
+  })
+  .catch((err) => {
+    console.log("db connection failed " + err);
+  });
+
+
+// app add routes
+
+app.use(convocatoriaRoutes);
+
+// init the server
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
