14d852e06ad682c8272265a229e6b0a891ba8ce0
Jairo José Sánchez Rodríguez <89935898+tpjairosanchez@users.noreply.github.com>
2021-12-01 17:47:48 -0500

    Update index.js
    Cambio para usar el puerto provisto por heroku

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ mongoose.connect("mongodb+srv:// @cluster0.6gpiz.mongodb.net/myFirstDatabase?ret
 app.use(convocatoriaRoutes);
 
 // init the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
